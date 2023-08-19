fbd7d573a0e471f0e9799cd2a6614638870fd29c
Loo Zi Xuan <zixuan@Loos-MBP.realtek>
2023-08-19 13:33:08 +0800

    adding custom font

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={{fontFamily: 'Inter-Black'}}>Hello World 123</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
