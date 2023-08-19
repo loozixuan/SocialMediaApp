90e3aa015ee3f9a1f74b39682e7d54f5e0441bc3
Loo Zi Xuan <zixuan@Loos-MBP.realtek>
2023-08-19 13:55:08 +0800

    Install Font Awesome Icons

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, ScrollView, View} from 'react-native';
 import Title from './components/Title/Title';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Title title={"Let's Explore"} />
+      <ScrollView>
+        <View
+          style={{
+            paddingTop: 30,
+            paddingHorizontal: 26,
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Title title={"Let's Explore"} />
+          <FontAwesomeIcon icon={faEnvelope} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
